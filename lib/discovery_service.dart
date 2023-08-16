e9c0afa7578cf8ec7e6cc515ee93b90565a4507d
Johan Flensmark <jokf@itu.dk>
2023-08-16 14:14:10 +0200

    Add stream based discovery & fix sometimes not resolving device

--- lib/discovery_service.dart
+++ lib/discovery_service.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:bonsoir/bonsoir.dart';
+import 'package:multicast_dns/multicast_dns.dart';
 
 import 'device.dart';
 
@@ -9,6 +10,7 @@ const _domain = '_googlecast._tcp';
 class CastDiscoveryService {
   static final CastDiscoveryService _instance = CastDiscoveryService._();
   CastDiscoveryService._();
+  final MDnsClient client = MDnsClient();
 
   factory CastDiscoveryService() {
     return _instance;
@@ -17,14 +19,76 @@ class CastDiscoveryService {
   Future<List<CastDevice>> search(
       {Duration timeout = const Duration(seconds: 5)}) async {
     final results = <CastDevice>[];
+    final discovery = BonsoirDiscovery(type: _domain);
 
+    await discovery.ready;
+    await client.start();
+    discovery.eventStream!.listen((event) async {
+      if (event.type == BonsoirDiscoveryEventType.DISCOVERY_SERVICE_RESOLVED ||
+          event.type == BonsoirDiscoveryEventType.DISCOVERY_SERVICE_FOUND) {
+        if (event.service == null || event.service?.attributes == null) {
+          return;
+        }
+
+        final port = event.service!.port;
+        final host = event.service?.toJson()['service.ip'];
+        String name = [
+          event.service?.attributes?['md'],
+          event.service?.attributes?['fn']
+        ].whereType<String>().join(' - ');
+        if (name.isEmpty) {
+          name = event.service!.name;
+        }
+
+        // Host is null on service found, so we need to resolve it
+        // Added this because the bonsoir library always correctly resolve the host
+        if (host == null) {
+          await for (SrvResourceRecord srv in client.lookup<SrvResourceRecord>(
+              ResourceRecordQuery.service(
+                  event.service!.name + "._googlecast._tcp.local"))) {
+            await for (IPAddressResourceRecord ip
+                in client.lookup<IPAddressResourceRecord>(
+                    ResourceRecordQuery.addressIPv4(srv.target))) {
+              results.add(CastDevice(
+                  name: srv.name,
+                  serviceName: srv.name,
+                  host: ip.address.address,
+                  port: srv.port));
+            }
+          }
+          return;
+        }
+        results.add(CastDevice(
+          serviceName: event.service!.name,
+          name: name,
+          port: port,
+          host: host,
+          extras: event.service!.attributes ?? {},
+        ));
+      }
+    });
+
+    await discovery.start();
+    await Future.delayed(timeout);
+    await discovery.stop();
+    client.stop();
+
+    return _dedupe(results);
+  }
+
+  Stream<List<CastDevice>> searchStream({
+    Duration timeout = const Duration(seconds: 5),
+  }) async* {
+    final results = <CastDevice>[];
     final discovery = BonsoirDiscovery(type: _domain);
     await discovery.ready;
-    await discovery.start();
+    await client.start();
 
-    discovery.eventStream!.listen((event) {
-      if (event.type == BonsoirDiscoveryEventType.discoveryServiceFound ||
-          event.type == BonsoirDiscoveryEventType.discoveryServiceResolved) {
+    final controller = StreamController<List<CastDevice>>();
+
+    discovery.eventStream!.listen((event) async {
+      if (event.type == BonsoirDiscoveryEventType.DISCOVERY_SERVICE_RESOLVED ||
+          event.type == BonsoirDiscoveryEventType.DISCOVERY_SERVICE_FOUND) {
         if (event.service == null || event.service?.attributes == null) {
           return;
         }
@@ -40,6 +104,22 @@ class CastDiscoveryService {
         }
 
         if (host == null) {
+          await for (SrvResourceRecord srv in client.lookup<SrvResourceRecord>(
+              ResourceRecordQuery.service(
+                  event.service!.name + "._googlecast._tcp.local"))) {
+            await for (IPAddressResourceRecord ip
+                in client.lookup<IPAddressResourceRecord>(
+                    ResourceRecordQuery.addressIPv4(srv.target))) {
+              final device = CastDevice(
+                name: srv.name,
+                serviceName: srv.name,
+                host: ip.address.address,
+                port: srv.port,
+              );
+              results.add(device);
+              controller.add(_dedupe(results)); // Emit updated list
+            }
+          }
           return;
         }
 
@@ -52,14 +132,33 @@ class CastDiscoveryService {
             extras: event.service!.attributes ?? {},
           ),
         );
+        controller.add(_dedupe(results)); // Emit updated list
       }
-    }, onError: (error) {
-      print('[CastDiscoveryService] error ${error.runtimeType} - $error');
     });
+    await discovery.start();
+    unawaited(Future.delayed(timeout).then((value) async {
+      controller.add(_dedupe(results));
+      controller.close();
+      await discovery.stop();
+      client.stop();
+    }));
 
-    await Future.delayed(timeout);
-    await discovery.stop();
-
-    return results.toSet().toList();
+    yield* controller.stream;
   }
+}
+
+//function that dedupes results
+List<CastDevice> _dedupe(List<CastDevice> results) {
+  final resultsMap = <String, CastDevice>{};
+  for (final result in results) {
+    final key = '${result.host}:${result.port}';
+    if (resultsMap.containsKey(key)) {
+      if (result.extras.isNotEmpty) {
+        resultsMap[key] = result;
+      }
+    } else {
+      resultsMap[key] = result;
+    }
+  }
+  return resultsMap.values.toList();
 }
